c5a77dc7d1f400deff1c6ff97e4ce311c38a74d5
youssefmansour14 <youssefmansour142000@gmail.com>
2024-03-01 21:22:54 +0200

    Add project files.

---
+++ Context/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolSystemAPI.Model;
+
+namespace SchoolSystemAPI.Context
+{
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
